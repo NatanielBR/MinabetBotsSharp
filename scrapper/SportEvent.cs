4494bd62ce87222ee47d04a6ad5dafcf6b4ca24b
Nataniel <natannada08@gmail.com>
2022-12-22 09:50:42 -0300

    Corrigido problema da Data

--- scrapper/SportEvent.cs
+++ scrapper/SportEvent.cs
@@ -2,16 +2,16 @@ namespace MinabetBotsWeb.scrapper;
 
 public class Event {
 
-    private string eventId;
-    private string champEventId;
-    private string championshipId;
-    private string championshipName;
-    private DateTimeOffset dateStarted;
-    private string teamHomeName;
-    private string teamAwayName;
-    private EventOdds odds;
-    private string sourceName;
-    private string url;
+    public readonly string eventId;
+    public readonly string champEventId;
+    public readonly string championshipId;
+    public readonly string championshipName;
+    public readonly DateTimeOffset? dateStarted;
+    public readonly string teamHomeName;
+    public readonly string teamAwayName;
+    public readonly EventOdds? odds;
+    public readonly string sourceName;
+    public readonly string url;
     
     public Event(string eventId, string champEventId, string championshipId, string championshipName, DateTimeOffset dateStarted, string teamHomeName, string teamAwayName, EventOdds odds, string sourceName, string url) {
         this.eventId = eventId;
@@ -25,6 +25,8 @@ public class Event {
         this.sourceName = sourceName;
         this.url = url;
     }
+    
+    
 
     public override string ToString() {
         return $"{nameof(eventId)}: {eventId}, {nameof(champEventId)}: {champEventId}, {nameof(championshipId)}: {championshipId}, {nameof(championshipName)}: {championshipName}, {nameof(dateStarted)}: {dateStarted}, {nameof(teamHomeName)}: {teamHomeName}, {nameof(teamAwayName)}: {teamAwayName}, {nameof(odds)}: {odds}, {nameof(sourceName)}: {sourceName}, {nameof(url)}: {url}";
@@ -32,11 +34,11 @@ public class Event {
 }
 
 public class EventOdds{
-    double home_win_odds;
-    double away_win_odds;
-    double draw_odds;
-    double more2and5odds;
-    double less2and5odds;
+    public readonly double home_win_odds;
+    public readonly double away_win_odds;
+    public readonly double draw_odds;
+    public readonly double more2and5odds;
+    public readonly double less2and5odds;
 
     public EventOdds(double homeWinOdds, double awayWinOdds, double drawOdds, double more2And5Odds, double less2And5Odds) {
         home_win_odds = homeWinOdds;
--- scrapper/soccer/BetsBola.cs
+++ scrapper/soccer/BetsBola.cs
@@ -1,14 +1,13 @@
 using System.Globalization;
 using System.Net;
-using System.Security.AccessControl;
 using System.Text.RegularExpressions;
-using System.Web;
 using HtmlAgilityPack;
 
 namespace MinabetBotsWeb.scrapper.soccer;
 
 public class BetsBola : BetApi {
     private HtmlWeb web = new();
+    private CultureInfo brazilCulture = new("pt-BR");
     private string urlBase = "https://betsbola.com/sistema_v2/usuarios/simulador/desktop/";
 
     public BetsBola(HttpClient client)
@@ -58,15 +57,18 @@ public class BetsBola : BetApi {
                         leagueName,
                         eventEl.SelectSingleNode("//div[@class='dateAndHour']").let(it => {
                             var date = it.SelectSingleNode("//span[@class='date']").InnerText;
+                            MonthToNumber.TryParse(date.Split("/")[1], true, out MonthToNumber monthNumber);
+
+                            // Irá transformar o nome do mês para o seu numero, o nome do mês esta em português
+                            date = $"{date.Split("/")[0]}/{(byte)monthNumber}";
 
                             var hour = it.SelectSingleNode("//span[@class='hour']").InnerText;
-                            
-                            // TODO: Tem que resolver isso
-                            return DateTimeOffset.Now;
-                            // return DateTimeOffset.ParseExact(
-                            //     $"{date}/{actualYear} {hour}:00 -3",
-                            //     "dd/MMM/yyyy hh:mm:ss t",
-                            //     new CultureInfo("pt-BR"));
+
+                            return DateTime.ParseExact($"{date} {hour}", "dd/MM HH:mm",
+                                brazilCulture, DateTimeStyles.None).let(time =>
+                                new DateTimeOffset(time,
+                                    TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time").GetUtcOffset(time))
+                            );
                         }),
                         "",
                         "",
@@ -81,7 +83,7 @@ public class BetsBola : BetApi {
                         ,
                         ""
                     );
-                    
+
                     events.Add(eventO);
                 });
             });
@@ -105,4 +107,19 @@ public class BetsBola : BetApi {
             .Select(url => regex.Match(url).Value.Replace("'.", "").Replace("'", ""))
             .ToList();
     }
+}
+
+enum MonthToNumber : byte {
+    JAN = 1,
+    FEV = 2,
+    MAR = 3,
+    ABR = 4,
+    MAI = 5,
+    JUN = 6,
+    JUL = 7,
+    AGO = 8,
+    SET = 9,
+    OUT = 10,
+    NOV = 11,
+    DEZ = 12
 }
