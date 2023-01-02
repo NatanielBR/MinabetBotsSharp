2006b3ddfd11e3ebd52cf61e27c7288892e8b680
Nataniel <natannada08@gmail.com>
2023-01-02 17:36:55 -0300

    Atualizado bot para usar o endpoint de eventsList ao invés de "event".

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Net;
-using System.Net.Http.Json;
 using System.Text;
 using MinabetBotsWeb.scrapper;
 using MinabetBotsWeb.scrapper.soccer;
@@ -8,29 +6,28 @@ using Newtonsoft.Json;
 
 namespace MinabetBotsWeb;
 
-public class Program
-{
+public class Program {
     private static HttpClient HttpClient = new();
-    
+
     [SuppressMessage("ReSharper.DPA", "DPA0002: Excessive memory allocations in SOH", MessageId = "type: HtmlAgilityPack.HtmlNode")]
     public static void Main(string[] args) {
         // See https://aka.ms/new-console-template for more information
 
-        var teamDb = new TeamDb(minRatio: 0.18, changeFire: 3);
+        var teamDb = new TeamDb(minRatio:0.18, changeFire:3);
         ProgramConfig programConfig;
 
         var configPath = args[0];
         Console.Out.WriteLine($"Checando se existe a config: '{configPath}'");
-        
+
         if (File.Exists(configPath)) {
             Console.Out.WriteLine("Arquivo de configuração existe. Carregando...");
             var text = File.ReadAllText(configPath);
             programConfig = JsonConvert.DeserializeObject<ProgramConfig>(text);
         } else {
             Console.Out.WriteLine("Arquivo de configuração não existe. Usando configurações padrões...");
-            programConfig = new();   
+            programConfig = new();
         }
-        
+
         var combinator = new Combinator(programConfig.EventType, teamDb);
         Console.Out.WriteLine("Carregado configuração:");
         Console.Out.WriteLine(programConfig.ToString());
@@ -43,7 +40,7 @@ public class Program
             jsonData.Headers.Add("bot-id", "aYvagG1zygWWKz0duUSj");
 
             // var result = 
-            await HttpClient.PostAsync($"{programConfig.DjangoUrl}/bot/events",
+            await HttpClient.PostAsync($"{programConfig.DjangoUrl}/bot/eventsList",
                 jsonData
             );
         };
@@ -65,7 +62,8 @@ public class Program
 
                     Console.Out.WriteLine($"Pegue: {elements.Count} itens");
                     teamDb.PutAll(elements);
-                } catch (Exception err) {
+                }
+                catch (Exception err) {
                     Console.Out.WriteLine("Falha ao pegar os dados");
                     Console.Out.WriteLine($"Erro: {err.Message}");
                     Console.Out.WriteLine($"StackTrace:\n{err.StackTrace}");
@@ -80,20 +78,20 @@ public class Program
 }
 
 public class ProgramConfig {
-    public string DjangoUrl { get; set; }
-    public string EventType { get; set; }
-    public int ThreadSleepMs { get; set; }
 
     public ProgramConfig() {
         DjangoUrl = "http://localhost:8000";
         EventType = "soccer";
         ThreadSleepMs = 4000;
     }
-    
+
     public ProgramConfig(string djangoUrl, string eventType) {
         DjangoUrl = djangoUrl;
         EventType = eventType;
     }
+    public string DjangoUrl { get; set; }
+    public string EventType { get; set; }
+    public int ThreadSleepMs { get; set; }
 
     public override string ToString() {
         return $"{nameof(DjangoUrl)}: {DjangoUrl}, {nameof(EventType)}: {EventType}, {nameof(ThreadSleepMs)}: {ThreadSleepMs}";
